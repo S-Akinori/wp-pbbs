41cd3c92cfee4d37d822f30966e0a8bcefac5632
akinori <aki10.working@gmail.com>
2023-09-03 18:44:54 +0900

    first commit

--- functions.php
+++ functions.php
@@ -2,4 +2,9 @@
 include TEMPLATEPATH . '/functions/generate-upload-image-tag.php';
 include TEMPLATEPATH . '/settings/custom-post-type.php';
 include TEMPLATEPATH . '/settings/company-settings.php';
+
 add_theme_support('post-thumbnails');
+// function theme_slug_setup() {
+//   add_theme_support( 'title-tag' );
+// }
+// add_action( 'after_setup_theme', 'theme_slug_setup' );
--- header.php
+++ header.php
@@ -3,13 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <meta property="og:title" content="Myクリニック" />
-  <meta property="og:description" content="クリニック向けテンプレートです" />
+  <title><?php bloginfo('name'); ?></title>
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <meta property="og:title" content="<?php bloginfo('name'); ?>" />
+  <meta property="og:description" content="<?php bloginfo('description'); ?>" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="<?php home_url(); ?>" />
   <meta property="og:image" content="<?= get_template_directory_uri(); ?>/image.png" />
-  <meta property="og:site_name" content="Myクリニック" />
+  <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
   <meta property="og:locale" content="ja_JP"  />
   <link rel="icon" href="/favicon.ico" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
