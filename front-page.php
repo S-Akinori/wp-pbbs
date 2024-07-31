d8718a79f4b9f56fd1d98920c3c3b5f1ee332340
akinori <aki10.working@gmail.com>
2024-07-31 15:24:26 +0900

    fix

--- front-page.php
+++ front-page.php
@@ -37,6 +37,8 @@ $shops = get_posts($args);
 $args = array(
   'post_type' => 'shop', // カスタム投稿タイプの名前
   'posts_per_page' => -1, // すべての投稿を表示する場合
+  'orderby'        => 'date', // 並べ替えの基準を日付に指定します。
+  'order'          => 'ASC', // 昇順に並べ替えます。
   'tax_query' => array(
     array(
       'taxonomy' => 'shop-cat',
